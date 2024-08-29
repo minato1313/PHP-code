034d0cd41ceb5e311e0de50d600e72934a64b6fa
Vishal Morchhle <143252661+minato1313@users.noreply.github.com>
2024-08-30 00:02:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,50 @@
-<html>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<body background="2.jpg" style="background-repeat:no-repeat;
-background-size: 100% 100%">
-<br><br><br><br>
-<div class="container">
-  <div class="jumbotron vertical-center">
-  	<table class="grid" cellspacing="0">
-  		<tr>
-  <td colspan="4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-  <td colspan="4">
-  	<form method="post">
-  <div class="form-group" action="post">
-    <label for="firstname">Name:</label>
-    <input type="text" class="form-control" name="firstname">
-  </div>
-  <div class="form-group">
-    <label for="email">Email:</label>
-    <input type="text" class="form-control" name="email">
-  </div>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button type="submit" class="btn btn-success">Submit</button>
-</form></td>  
-  <td colspan="4"></td>
-</tr>
-</table>
-</div>
-</div>
 <?php
-$firstname=$_POST['firstname'];
-$email=$_POST['email'];
-$servername = "intelli.coghw13fheqo.us-east-2.rds.amazonaws.com";
-$username = "intel";
-$password = "intel123";
-$db = "intel";
+// Database connection
+$servername = "your-rds-endpoint";
+$username = "your-username";
+$password = "your-password";
+$dbname = "mydatabase";
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-if(isset($_POST['firstname']) && isset($_POST['email'])){
-$sql = "INSERT INTO data (firstname,email)
-VALUES ('".$firstname."', '".$email."')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    // Prepare and bind
+    $stmt = $conn->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
+    $stmt->bind_param("ss", $name, $email);
+
+    // Execute
+    if ($stmt->execute()) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
 }
 
 $conn->close();
-}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert Name and Email</title>
+</head>
+<body>
+    <h2>Insert Name and Email</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Name: <input type="text" name="name" required><br><br>
+        Email: <input type="email" name="email" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
